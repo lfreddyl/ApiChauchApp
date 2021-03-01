83b5a832cf4fe3903ae12045c440447348e87adf
lfreddyl <orlanjack95@gmail.com>
2021-03-01 17:01:42 -0500

    configuracion Bd

--- global/env.ts
+++ global/env.ts
@@ -1,5 +1,5 @@
 export const SERVER_PORT:number=Number(process.env.PORT||5000);
 
 
-export const URL_MONGODB='mongodb+srv://usuario:password@ejemplo.u0y90.mongodb.net/BD'
+export const URL_MONGODB='mongodb+srv://rolo:rolo123@cluster0.n4uid.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
